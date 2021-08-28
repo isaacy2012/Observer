85351603c36e79b7ea8f18520c5685cffaeece06
georgepnz <86088842+georgepnz@users.noreply.github.com>
2021-08-28 19:01:07 +1200

    MongoDB connected

--- server/app.js
+++ server/app.js
@@ -3,27 +3,19 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require("cors");
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const cors = require('cors')
 const testAPIRouter = require("./routes/testAPI");
+const { MongoClient } = require('mongodb');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-app.use(cors())
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use("/testAPI", testAPIRouter);
 
 // catch 404 and forward to error handler
@@ -43,3 +35,16 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+//Setting up MongoDB
+const uri = "mongodb+srv://dbUser:mO6XYe70n1Htw08A@cluster0.n3bdm.mongodb.net/Observer?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+c();
+async function c(){
+  const collection = (await client.connect()).db("Observer").collection("Messages");
+  const doc = {name: "Neapolitan pizza", shape: "round"};
+  const result = await collection.insertOne(doc);
+  console.log(
+      `A document was inserted with the _id: ${result.insertedId}`,);
+  await client.close();
+}
