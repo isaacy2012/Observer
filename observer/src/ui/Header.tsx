e1ee8e0dd6575bc57bb92259c59d83743113fbf3
isaacy2012 <50868057+isaacy2012@users.noreply.github.com>
2021-09-06 21:46:39 +1200

    Fixed UI so that PIN is below the Observer logo

--- observer/src/ui/Header.tsx
+++ observer/src/ui/Header.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import "../css/App.css"
 import "../css/Header.css"
 
-export function Header(props: {logo: JSX.Element, roomPin: number, title: String, originalPosterName: String}) {
+export function Header(props: { logo: JSX.Element, roomPin: number, title: String, originalPosterName: String }) {
     return (
         <div className="header">
             <div className="row">
                 <div className="column leftColumn">
                     {props.logo}
-                    <h3>{"PIN: " + props.roomPin}</h3>
+                    <h3 className="pin-text">{"PIN: " + props.roomPin}</h3>
                 </div>
                 <div className="column middleColumn">
                     <h1>{props.title}</h1>
